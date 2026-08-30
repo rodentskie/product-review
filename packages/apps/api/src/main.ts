f984c77aec1cf63184c3d9fc7a18974e2e1b8a02
rodentskie <rodentskie@gmail.com>
2026-08-30 08:13:45 +0800

    fix: cors

--- packages/apps/api/src/main.ts
+++ packages/apps/api/src/main.ts
@@ -11,7 +11,7 @@ const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
